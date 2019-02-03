236be7277e8947465fdc2b8189b54414c9597b33
Jorge Saud <giorgiosaud@gmail.com>
2019-02-03 19:08:24 -0300

    add structure and transactions details (#1)

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    public function account()
+    public function originAccount()
     {
-        return $this->belongsTo(Account::class);
+        return $this->belongsTo(Account::class, 'from_account_id');
+    }
+    public function destinationAccount()
+    {
+        return $this->belongsTo(Account::class, 'to_account_id');
     }
     //
 }
