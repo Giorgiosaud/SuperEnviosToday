de8ffe8b008b1cfed483cc104f9df57e592c87ba
Jorge Saud <giorgiosaud@gmail.com>
2019-02-03 19:28:49 -0300

    Devlop (#9)
    * add structure and transactions details (#2)
    
    * Apply fixes from StyleCI (#5)
    
    * Apply fixes from StyleCI (#8)

--- app/Transaction.php
+++ app/Transaction.php
@@ -10,6 +10,7 @@ class Transaction extends Model
     {
         return $this->belongsTo(Account::class, 'from_account_id');
     }
+
     public function destinationAccount()
     {
         return $this->belongsTo(Account::class, 'to_account_id');
