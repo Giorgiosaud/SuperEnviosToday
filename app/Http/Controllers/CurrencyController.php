ab661e3129c092e2b4183f06ea7dbdb19ce9ebcc
Jorge Saud <giorgiosaud@gmail.com>
2019-02-06 02:45:30 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -30,7 +30,8 @@ class CurrencyController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,7 +42,8 @@ class CurrencyController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Currency  $currency
+     * @param \App\Currency $currency
+     *
      * @return \Illuminate\Http\Response
      */
     public function show(Currency $currency)
@@ -52,7 +54,8 @@ class CurrencyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Currency  $currency
+     * @param \App\Currency $currency
+     *
      * @return \Illuminate\Http\Response
      */
     public function edit(Currency $currency)
@@ -63,8 +66,9 @@ class CurrencyController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Currency  $currency
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Currency            $currency
+     *
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Currency $currency)
@@ -75,7 +79,8 @@ class CurrencyController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Currency  $currency
+     * @param \App\Currency $currency
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy(Currency $currency)
