74469ddcefc06e30b2687ea5c809a4541289a345
Jorge Saud <jorgelsaud@gmail.com>
2019-02-11 00:10:24 -0300

    base migrations and routes

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -24,5 +24,8 @@ class RoleSeeder extends Seeder
         Role::create([
             'name' => 'client',
         ]);
+        Role::create([
+            'name' => 'receiver',
+        ]);
     }
 }
