17d0f51456da225fd7b52e16e065d7272959f19a
Jorge Saud <jorgelsaud@gmail.com>
2019-02-17 14:00:30 -0300

    users fixes

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,6 +6,10 @@ use App\User;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
 
+/**
+ * Class UserTest
+ * @package Tests\Feature
+ */
 class UserTest extends TestCase
 {
     /**
@@ -32,6 +36,28 @@ class UserTest extends TestCase
         $response = $this->get('/api/users');
         $response->assertJsonCount(11, $key = 'data');
 
+    }
+
+    /**
+     * @test
+     */
+    public function rolesAPIWorks(){
+        $user = factory(User::class)->create([
+            'name' => 'Coordinador',
+            'idn' => '1',
+            'idn_type' => 'CI',
+            'password' => bcrypt('hidden'),
+        ]);
+        $user->toogleRole('coordinator');
+        Passport::actingAs(
+            $user,
+            ['create-servers']
+        );
+        $response = $this->get('/api/roles');
+        $response->assertJsonCount(4, $key = 'data');
+
         $response->assertStatus(200);
+
     }
 }
+
