c983f593ddcfc45b61619415012f4d73994f5739
Giorgiosaud <jsaud@modyo.com>
2019-05-11 20:06:46 -0400

    translate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+<<<<<<< HEAD
+=======
+
+>>>>>>> cae4c848da4b2b6aa0c27cb1c7f44e127596f31a
     <title>{{ config('app.name', 'Super Envios Today') }}</title>
 
     <!-- Scripts -->
@@ -87,7 +91,8 @@
                                     <a class="dropdown-item" href="{{route('my_transactions')}}">Mis
                                         Transacciones
                                         </a>
-                                    <a class="dropdown-item" href="{{route('chilean_pending_transactions')}}">Mis Transacciones Pendientes</a>
+                                    <a class="dropdown-item" href
+                                    ="{{route('chilean_pending_transactions')}}">Mis Transacciones Pendientes</a>
 
                                 </div>
                             </li>
