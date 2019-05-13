ab66f97c67626593f38a44336397a24118c5084a
Giorgiosaud <jsaud@modyo.com>
2019-05-13 14:18:21 -0400

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -175,7 +175,7 @@
             })->get();
         }
         public function aliasing($id){
-          return $id;
+          return User::find($id);
           Auth::login($user);
           return redirect('/');
         }
