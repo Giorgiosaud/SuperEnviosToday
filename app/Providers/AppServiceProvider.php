674eeb600e2cd6381bded513450f8231652d62c5
Giorgiosaud <jsaud@modyo.com>
2019-06-29 23:26:05 -0400

    fix schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('vendor.pagination.bootstrap-4');
-
+        Schema::defaultStringLength(191);
         //User::observe(UserObserver::class);
 
         //
