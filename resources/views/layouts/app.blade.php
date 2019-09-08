fa1f31c1ba8577fb6a49f41258298657b8f22473
Giorgiosaud <jsaud@modyo.com>
2019-09-08 17:30:29 -0300

    fix menu name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,8 +54,7 @@
                                 <a class="dropdown-item" href="{{route('registerOperator')}}">Registrar
                                     Operador</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('registerOperatorAccount')}}">Agregar Cuenta
-                                    para Operador </a>
+                                <a class="dropdown-item" href="{{route('registerOperatorAccount')}}">Agregar Cuenta</a>
                                     <a class="dropdown-item" href="{{route('assignOperatorAccount')}}">Asignar Operador a cuenta</a>
                                 <a class="dropdown-item" href="{{route('addFoundsToVenezuelanOperator')}}">Agregar
                                     Fondos a Operador Venezuela</a>
