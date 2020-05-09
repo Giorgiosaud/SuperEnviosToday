31701be70e91ca99642eca60721ced1ce1128fc5
Jorge Saud <jsaud@modyo.com>
2020-05-09 15:57:37 -0400

    gitignore fix

---
+++ resources/lang/es/transaction.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'TITLE'=>'Crear Transacción',
+    'MESSAGE'=>'Aqui puedes gestionar una transacción de :app',
+    'CLIENT:TITLE'=>'Datos del Cliente Emisor',
+    'TRANSACTION:TITLE'=>'Datos de la transacción',
+    'RECEIVER:TITLE'=>'Datos del receptor',
+    'VENEZUELAN_OPERATOR:TITLE'=>'Selección del operador venezuela',
+    'REVIEW:TITLE'=>'Revisar Transacción',
+    'SEARCH:LABEL'=>'Buscar o crear:'
+];
