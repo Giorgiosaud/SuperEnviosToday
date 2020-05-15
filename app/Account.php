b5d371d2b8bc2a440bffaf9ed4998aefaefebeaa
Jorge Saud <jsaud@modyo.com>
2020-05-15 00:09:31 -0400

    refactor: optimize balance calc

--- app/Account.php
+++ app/Account.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
-
 /**
  * App\Account.
  *
@@ -56,6 +55,10 @@ class Account extends Model
     {
         return $this->hasMany(Transaction::class, 'to_account_id');
     }
+  
+    public function balanceCache(){
+      return $this->hasOne(BalanceCache::class);
+    }
 
     /**
      * @return HasMany
@@ -72,26 +75,52 @@ class Account extends Model
     {
         return $this->hasMany(Transaction::class, ['from_account_id','to_account_id']);
     }
-
     /**
      * @return HasMany
      */
-    public function outgoingTransactionsTyped()
+    public function outgoingTransactionsTyped($lastTransactionId)
     {
-        return $this->outgoingTransactions()->where('type', 'outcome');
+      if($lastTransactionId){
+        return $this->outgoingTransactions()->where('id','>',$lastTransactionId)->where('type', 'outcome');
+      }
+      return $this->outgoingTransactions()->where('type', 'outcome');
     }
 
-    public function incomingTransactionsTyped()
+    public function incomingTransactionsTyped($lastTransactionId)
     {
-        return $this->incomingTransactions()->where('type', 'income');
+      if($lastTransactionId){
+        return $this->incomingTransactions()->where('id','>',$lastTransactionId)->where('type', 'income');
+      }
+      return $this->incomingTransactions()->where('type', 'income');
     }
-
     /**
      * @return mixed
      */
     public function getBalanceAttribute()
     {
-        return $this->incomingTransactionsTyped->sum('amount') - $this->outgoingTransactionsTyped->sum('amount');
+      $cachedBalanceTransactionId= $this->balanceCache?$this->balanceCache->transaction_id:null;
+      
+      $incomingTransactionsNoCached=$this->incomingTransactionsTyped($cachedBalanceTransactionId)->get();
+      
+      $outgoingTransactionsNoCached=$this->outgoingTransactionsTyped($cachedBalanceTransactionId)->get();
+      $sumIncomings=$incomingTransactionsNoCached->sum('amount');
+      $sumOutgoings=$outgoingTransactionsNoCached->sum('amount');
+      $cache_balance_diff_amount = $sumIncomings - $sumOutgoings;
+      $cachedBalanceAmount= $cachedBalanceTransactionId?$this->balanceCache->amount:0;
+      $newCachedBalance=$cachedBalanceAmount+$cache_balance_diff_amount;
+      if($incomingTransactionsNoCached->count()+$outgoingTransactionsNoCached->count()>15){
+        $maxId=max($incomingTransactionsNoCached->last()->id,$outgoingTransactionsNoCached->last()->id);
+        if($cachedBalanceTransactionId){
+          $this->balanceCache->transaction_id=$maxId;
+          
+          $this->balanceCache->amount=strval($newCachedBalance);
+          $this->save();
+        }else{
+          $this->balanceCache()->create(['transaction_id'=>$maxId,'amount'=>strval($newCachedBalance)]);
+        }
+        return $newCachedBalance;
+      }
+        return $newCachedBalance;
     }
 
     /**
