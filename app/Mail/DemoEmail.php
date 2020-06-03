62c50d5eeda5c3c1def83e72115af06749742689
Jorge Saud <jsaud@modyo.com>
2020-06-03 15:37:17 -0400

    feat(email): demo email

--- app/Mail/DemoEmail.php
+++ app/Mail/DemoEmail.php
@@ -38,10 +38,6 @@ class DemoEmail extends Mailable
                 [
                     'testVarOne' => '1',
                     'testVarTwo' => '2',
-                ])
-            ->attach(public_path('/images').'/demo.jpg', [
-                'as' => 'demo.jpg',
-                'mime' => 'image/jpeg',
-            ]);
+                ]);
     }
 }
