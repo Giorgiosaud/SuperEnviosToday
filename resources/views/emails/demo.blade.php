6741f6203bc4cc5be550d96e4e3e38ed142c0a08
Jorge Saud <jsaud@modyo.com>
2020-06-03 15:35:48 -0400

    feat(email): demo email

--- resources/views/emails/demo.blade.php
+++ resources/views/emails/demo.blade.php
@@ -1,8 +1,20 @@
-@component('mail::message')
-    # Introduction
+Hello <i>{{ $demo->receiver }}</i>,
+<p>This is a demo email for testing purposes! Also, it's the HTML version.</p>
 
-    The body of your message.
+<p><u>Demo object values:</u></p>
 
-    Thanks,<br>
-    {{ config('app.name') }}
-@endcomponent
+<div>
+    <p><b>Demo One:</b>&nbsp;{{ $demo->demo_one }}</p>
+    <p><b>Demo Two:</b>&nbsp;{{ $demo->demo_two }}</p>
+</div>
+
+<p><u>Values passed by With method:</u></p>
+
+<div>
+    <p><b>testVarOne:</b>&nbsp;{{ $testVarOne }}</p>
+    <p><b>testVarTwo:</b>&nbsp;{{ $testVarTwo }}</p>
+</div>
+
+Thank You,
+<br/>
+<i>{{ $demo->sender }}</i>
