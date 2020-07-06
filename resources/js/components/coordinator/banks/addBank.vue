b5209941c0835909159e5fe6d060f8e7e1f0f4a4
Jorge Saud <jsaud@modyo.com>
2020-07-05 22:49:44 -0400

    fix axe on bank

--- resources/js/components/coordinator/banks/addBank.vue
+++ resources/js/components/coordinator/banks/addBank.vue
@@ -25,8 +25,8 @@
 
             </section>
             <footer class="modal-card-foot">
-                <button class="button" type="button" @click="$parent.close()">Cerrar</button>
-                <button class="button is-primary" @click="newBank">Guardar</button>
+                <button class="button" type="button" @click="$parent.close()" @keydown.esc="$parent.close()">Cerrar</button>
+                <button class="button is-primary" @click="newBank" @keypress.enter="newBank">Guardar</button>
             </footer>
         </div>
     </div>
