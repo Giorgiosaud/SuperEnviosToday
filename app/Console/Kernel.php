d31c6e7629924b3a6c71da88c4b6d67afa3d4916
Jorge Saud <jsaud@modyo.com>
2020-10-24 15:19:29 -0300

    feat: remove jobs to artisan run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,9 +25,7 @@ class Kernel extends ConsoleKernel
   protected function schedule(Schedule $schedule)
   {
     $schedule->command('telescope:prune --hours=48')->daily();
-    $schedule->command('queue:work --tries=3 --stop-when-empty')
-      ->cron('* * * * * *')
-      ->withoutOverlapping();
+  
     $schedule->command('passport:purge')->daily();
   }
 
