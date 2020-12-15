3d94c51f4ace87df91505b1b491010ac3f4039dc
Jorge Saud <jsaud@modyo.com>
2020-12-15 18:18:16 -0300

    fix: mail data

--- app/Mail/Test.php
+++ app/Mail/Test.php
@@ -3,31 +3,58 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
 class Test extends Mailable
 {
-    use Queueable, SerializesModels;
+  use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  /**
+   * Create a new message instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    //
+  }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->markdown('emails.test');
-    }
+  /**
+   * Build the message.
+   *
+   * @return $this
+   */
+  public function build()
+  {
+    $headerData = [
+      'category' => 'test',
+      'unique_args' => [
+        'variable_1' => 'email'
+      ]
+    ];
+
+    $header = $this->asString($headerData);
+
+    $this->withSwiftMessage(function ($message) use ($header) {
+      $message->getHeaders()
+        ->addTextHeader('X-SMTPAPI', $header);
+    });
+    return $this->markdown('emails.test');
+  }
+
+  private function asString($data)
+  {
+    $json = $this->asJSON($data);
+
+    return wordwrap($json, 76, "\n   ");
+  }
+
+  private function asJSON($data)
+  {
+    $json = json_encode($data);
+    $json = preg_replace('/(["\]}])([,:])(["\[{])/', '$1$2 $3', $json);
+
+    return $json;
+  }
 }
